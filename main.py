c2b3059bb2e158afa01dca78ed999570b2a332d0
Soroush Nikjou <sorushn@gmail.com>
2024-07-17 22:51:12 +0200

    pylint suggestions

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import numpy as np
-import scipy.signal as signal
+from scipy import signal
 
 
 class GameOfLife:
+    """
+    Conway's Game of Life
+    """
     kernel = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
 
     def __init__(self, state=None, grid_shape=10):
+        """
+        Initialize the GameOfLife object.
+
+        Args:
+            state (numpy.ndarray, optional): The initial state of the game. Defaults to None.
+            grid_shape (int, optional): The shape of the game grid. Defaults to 10.
+
+        Returns:
+            None
+        """
         self.grid_shape = grid_shape
         if state is None:
             self.grid = np.random.randint(0, 2, (grid_shape, grid_shape))
@@ -15,7 +28,17 @@ class GameOfLife:
         # self.grid[3:6, 3] = 1
 
     def update(self):
-        neighbor_count = signal.convolve2d(self.grid, self.kernel, mode="same")
+        """
+        Updates the state of the GameOfLife grid based on Conway's Game of Life rules.
+
+        Args:
+            self: The GameOfLife object.
+
+        Returns:
+            None
+        """
+        neighbor_count = signal.signal.convolve2d(
+            self.grid, self.kernel, mode="same")
         ones = np.multiply(neighbor_count, self.grid)
         ones[ones < 2] = 0
         ones[ones > 3] = 0
@@ -27,19 +50,23 @@ class GameOfLife:
         zeros[zeros == 3] = 1
         self.grid = np.add(ones, zeros)
 
-    def get_grid(self):
-        return self.grid
-
-    def get_state(self):
-        return self.grid
-
-    def set_state(self, state):
-        self.grid = state
-
     def reset(self):
+        """
+        Resets the grid of the GameOfLife object with random integers 0 or 1.
+        """
         self.grid = np.random.randint(0, 2, self.grid.shape)
 
     def print_board(self, truncate=True, symbols=['□ ', '■ ']):
+        """
+        Prints the GameOfLife grid to the console.
+
+        Args:
+            truncate (bool, optional): Whether to truncate the grid to the bounding box of live cells. Defaults to True.
+            symbols (List[str], optional): The symbols to use for representing live and dead cells. Defaults to ['□ ', '■ '].
+
+        Returns:
+            None
+        """
         if truncate:
             x, y = np.where(self.grid == 1)
             temp_grid = self.grid[min(x):max(x)+1, min(y):max(y)+1]
@@ -57,10 +84,10 @@ class GameOfLife:
 
 if __name__ == '__main__':
     game = GameOfLife()
-    round_count = 0
+    round_counter = 0
     while True:
-        print(f'Round {round_count}:')
+        print(f'Round {round_counter}:')
         game.print_board(False)
         game.update()
-        round_count += 1
+        round_counter += 1
         input()
